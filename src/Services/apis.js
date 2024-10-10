900d887a662eb8dcdc1768265e4751ec992eee1a
Deepak Kumar Bansal <122824965+deepakkumarbansal@users.noreply.github.com>
2024-10-10 14:39:09 +0530

    Update apis.js

--- src/Services/apis.js
+++ src/Services/apis.js
@@ -1,50 +1,53 @@
-const BASE_URL = "http://localhost:8400/api";
+import { envConfig } from "../config/envConfig";
+const {baseBackendUrl : BASE_URL} = envConfig
 
 export const profileEndpoints = {
-  GET_ALL_USER_API: BASE_URL + "/admin/all-users",
-  GET_ALL_REPORTERS_API: BASE_URL + "/admin/all-reporters",
+  GET_ALL_USER_API: BASE_URL + "/api/admin/all-users",
+  GET_ALL_REPORTERS_API: BASE_URL + "/api/admin/all-reporters",
   UPDATE_STATUS_OF_USER_API: (userId) =>
-    BASE_URL + `/admin/update-status/${userId}`,
-  GER_ALL_ARTICLES: BASE_URL + "/admin/all-articles",
+    BASE_URL + `/api/admin/update-status/${userId}`,
+  GER_ALL_ARTICLES: BASE_URL + "/api/admin/all-articles",
   UPDATE_ARTICLE_VERIFICATION: (articleId) =>
-    BASE_URL + `/admin/verify-article/${articleId}`,
+    BASE_URL + `/api/admin/verify-article/${articleId}`,
   UPDATE_ARTICLE_STATUS: (articleId) =>
-    BASE_URL + `/admin/update-article-status/${articleId}`,
+    BASE_URL + `/api/admin/update-article-status/${articleId}`,
 };
 
 export const articlesEndPoints = {
-  CREATE_ARTICLE: BASE_URL + "/article/create",
-  GET_ALL_ARTICLE: BASE_URL + "/article/all-articles",
-  GET_ALL_ADMIN_ARTICLE: BASE_URL + "/admin/all-articles",
+  CREATE_ARTICLE: BASE_URL + "/api/article/create",
+  GET_ALL_ARTICLE: BASE_URL + "/api/article/all-articles",
+  GET_ALL_ADMIN_ARTICLE: BASE_URL + "/api/admin/all-articles",
   GET_ARTICLE_BY_ID: (articleId) =>
-    BASE_URL + `/article/whole-article/${articleId}`,
+    BASE_URL + `/api/article/whole-article/${articleId}`,
   UPDATE_ARTICLE_BY_ID: (articleId) =>
-    BASE_URL + `/article/update-article/${articleId}`,
+    BASE_URL + `/api/article/update-article/${articleId}`,
   DELETE_ARTICLE_BY_ID: (articleId) =>
-    BASE_URL + `/article/delete-article/${articleId}`,
+    BASE_URL + `/api/article/delete-article/${articleId}`,
   GET_ARTICLE_BY_REPORTERS_ID: (articleId) =>
-    BASE_URL + `/article/reporter-articels/${articleId}`,
-  GET_ARTICLES_BY_CATAGORY: BASE_URL + `/article/article-category`,
-  SEARCH_ARTICLES: BASE_URL + `/article/search-article`
+    BASE_URL + `/api/article/reporter-articels/${articleId}`,
+  GET_ARTICLES_BY_CATAGORY: BASE_URL + `/api/article/article-category`,
+  SEARCH_ARTICLES: BASE_URL + `/api/article/search-article`
 };
+
 export const commentsEndPoints = {
-  CREATE_COMMENT: BASE_URL + "/comment/create",
+  CREATE_COMMENT: BASE_URL + "/api/comment/create",
   GET_COMMENT_BY_ARTICLE_ID: (articleId) =>
-    BASE_URL + `/comment/getCommentsByArticle/:${articleId}`,
+    BASE_URL + `/api/comment/getCommentsByArticle/:${articleId}`,
 };
+
 export const authEndPoints = {
-  LOGIN_API: BASE_URL + "/auth/login",
-  REGISTER_API: BASE_URL + "/auth/register",
-  FORGET_PASSWORD_API: BASE_URL + "/auth/forgetpassword",
-  VERIFY_PASSWORD_API: BASE_URL + "/auth/verifypassword",
-  UPDATE_PASSWORD: BASE_URL + `/auth/changePassword`,
-  UPDATE_NAME: BASE_URL + `/auth/changeName`,
-  SEND_INVITATION_MAIL: BASE_URL + `/admin/invite-reporter`,
-  ACCEPT_INVITE_REPORTER: (token) => BASE_URL + `/admin/accept-invite-reporter/${token}`,
-  UPDATE_AVATAR_URL: BASE_URL + `/auth/update-avatar-url`
+  LOGIN_API: BASE_URL + "/api/auth/login",
+  REGISTER_API: BASE_URL + "/api/auth/register",
+  FORGET_PASSWORD_API: BASE_URL + "/api/auth/forgetpassword",
+  VERIFY_PASSWORD_API: BASE_URL + "/api/auth/verifypassword",
+  UPDATE_PASSWORD: BASE_URL + `/api/auth/changePassword`,
+  UPDATE_NAME: BASE_URL + `/api/auth/changeName`,
+  SEND_INVITATION_MAIL: BASE_URL + `/api/admin/invite-reporter`,
+  ACCEPT_INVITE_REPORTER: (token) => BASE_URL + `/api/admin/accept-invite-reporter/${token}`,
+  UPDATE_AVATAR_URL: BASE_URL + `/api/auth/update-avatar-url`
 };
 
 export const visitorCountApi = {
-  getCount: BASE_URL + "/count/visit-count",
-  updateCount: BASE_URL + "/count/update-visit-count",
+  getCount: BASE_URL + "/api/count/visit-count",
+  updateCount: BASE_URL + "/api/count/update-visit-count",
 };
